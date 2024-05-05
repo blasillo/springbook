00548486b906b1bdcab7bf9ca8b1887405dbc201
blas <blasillous@gmail.com>
2024-05-05 21:47:53 +0200

    frontend angular modulos componentes

--- frontend-ui/src/app/modules/book/book.module.ts
+++ frontend-ui/src/app/modules/book/book.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FormsModule} from "@angular/forms";
 
+import { BookRoutingModule } from './book-routing.module';
+import { MainComponent } from './pages/main/main.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainComponent,
+    MenuComponent
+
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    BookRoutingModule,
+    FormsModule
   ]
 })
 export class BookModule { }
